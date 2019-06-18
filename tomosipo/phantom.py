c3341d7549317bd426046b18c6c7ba0f323039fc
Allard Hendriksen <allard.hendriksen@cwi.nl>
2019-06-18 13:28:32 +0200

    Add hollow box phantom

---
+++ tomosipo/phantom.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+
+def hollow_box(vd):
+    """Fills a volume dataset with a hollow box phantom
+
+    :param vd: `ts.Data`
+        A volume dataset.
+    :returns:
+    :rtype: ts.Data
+
+    """
+    vol = vd.get()
+    shape = np.array(vol.shape)
+
+    s20 = shape * 20 // 100
+    s40 = shape * 40 // 100
+
+    box_slices = tuple(slice(a, l - a) for (a, l) in zip(s20, shape))
+    hollow_slices = tuple(slice(a, l - a) for (a, l) in zip(s40, shape))
+
+    vol[:] = 0.0
+    vol[box_slices] = 1.0
+    vol[hollow_slices] = 0.0
+
+    return vd

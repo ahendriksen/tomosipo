8f5c20f9dd5414b4c6cd0b94875a4221d54051b5
Allard Hendriksen <allard@allardhendriksen.nl>
2019-07-01 20:26:17 +0200

    WIP: Split Data into Data and Backend
    There are two reasons to do this:
    
    1) We create the numpy arrays ourselves, instead of calling
       `astra.data3d.create`. If you call astra.data3d.delete on astra
       managed data, it is freed. This can be problematic if you still
       have a reference to the numpy array:
       >>> with ts.data(ts.volume(shape=256), 0.0) as vd:
       >>>     v = vd.data
       >>> # vd is freed (astra.data3d.delete)
       >>> v[:] = 1.0 		# segfault
    
       With this patch, a numpy array is created and linked, irrespective
       of the initial_value. Astra is smart about it, and instead of
       freeing the memory, it just decrements the refcount. This will free
       the array when there are no references left and makes the api a lot
       less surprising in general.
    
    2) By separating out the backend, we open up the possibility of
       supporting all kinds of other backends, such as pytorch. Moreover,
       we can very easily support data on the GPU, because the `data`
       property is separate from the `get_linkable_array`, thus allowing
       the backend to expose a gpuarray to astra, while exposing its own
       API to the programmer.
    
    *NB*: Point 1 may not hold for gpuarrays. This will have to be
          investigated.

--- tomosipo/Data.py
+++ tomosipo/Data.py
@@ -59,53 +59,24 @@ class Data(object):
         super(Data, self).__init__()
         self.geometry = geometry
         self.initial_value = initial_value
-
-        astra_type_dict = {
-            "vol": "-vol",
-            "vol_link": "-vol",
-            "sino": "-proj3d",
-            "sino_link": "-sino",
-        }
+        self.astra_geom = geometry.to_astra()
 
         if self.is_volume():
-            self.astra_geom = geometry.to_astra()
-            self.data_type = "vol"
+            astra_data_type = "-vol"
+            shape = geometry.shape
         elif self.is_projection():
-            self.astra_geom = geometry.to_astra()
-            self.data_type = "sino"
+            astra_data_type = "-sino"
+            shape = (geometry.shape[0], geometry.num_angles, geometry.shape[1])
         else:
             raise ValueError(
                 f"Geometry '{geometry.__class__}' is not supported. Cannot determine if volume or projection geometry."
             )
 
-        if initial_value is None:
-            astra_type = astra_type_dict[self.data_type]
-            self.astra_id = astra.data3d.create(astra_type, self.astra_geom, 0.0)
-        elif np.isscalar(initial_value):
-            astra_type = astra_type_dict[self.data_type]
-            self.astra_id = astra.data3d.create(
-                astra_type, self.astra_geom, initial_value
-            )
-        else:
-            # Make contiguous:
-            if initial_value.dtype != np.float32:
-                warnings.warn(
-                    f"The parameter initial_value is of type {initial_value.dtype}; expected `np.float32`. "
-                    f"The type has been Automatically converted."
-                )
-                initial_value = initial_value.astype(np.float32)
-            if not (
-                initial_value.flags["C_CONTIGUOUS"] and initial_value.flags["ALIGNED"]
-            ):
-                warnings.warn(
-                    f"The parameter initial_value should be C_CONTIGUOUS and ALIGNED."
-                    f"It has been automatically made contiguous and aligned."
-                )
-                initial_value = np.ascontiguousarray(initial_value)
-            astra_type = astra_type_dict[self.data_type + "_link"]
-            self.astra_id = astra.data3d.link(
-                astra_type, self.astra_geom, initial_value
-            )
+        self._backend = NumpyBackend(shape, initial_value)
+
+        self.astra_id = astra.data3d.link(
+            astra_data_type, self.astra_geom, self._backend.get_linkable_array()
+        )
 
     def clone(self):
         """Clone Data object
@@ -128,6 +99,85 @@ class Data(object):
 
     @property
     def data(self):
+        """Returns the underlying data.
+
+        Changes to the return value will be reflected in the astra
+        data.
+
+        If you want to avoid this, consider copying the data
+        immediately, using `np.copy` for instance.
+
+        NOTE: if this data encapsulates projection data, the order of
+        the axes is (V, num_angles, U).
+
+        :returns: The underlying data object. This can be a numpy
+        array or some other type of data.
+
+        :rtype: np.array
+
+        """
+        return self._backend.data
+
+    @data.setter
+    def data(self, val):
+        self._backend.data = val
+
+    def is_volume(self):
+        return ts.geometry.is_volume(self.geometry)
+
+    def is_projection(self):
+        return ts.geometry.is_projection(self.geometry)
+
+    def to_astra(self):
+        """Returns astra data id associated with current object
+
+        :returns:
+        :rtype:
+
+        """
+        return self.astra_id
+
+
+
+class NumpyBackend(object):
+    """Documentation for NumpyBackend
+
+    """
+    def __init__(self, shape, initial_value):
+        super(NumpyBackend, self).__init__()
+        self._shape = shape
+
+
+        if initial_value is None:
+            self._data = np.zeros(shape, dtype=np.float32)
+        elif np.isscalar(initial_value):
+            self._data = np.zeros(shape, dtype=np.float32)
+            self._data[:] = initial_value
+        else:
+            initial_value = np.array(initial_value, copy=False)
+            # Make contiguous:
+            if initial_value.dtype != np.float32:
+                warnings.warn(
+                    f"The parameter initial_value is of type {initial_value.dtype}; expected `np.float32`. "
+                    f"The type has been Automatically converted."
+                )
+                initial_value = initial_value.astype(np.float32)
+            if not (
+                initial_value.flags["C_CONTIGUOUS"] and initial_value.flags["ALIGNED"]
+            ):
+                warnings.warn(
+                    f"The parameter initial_value should be C_CONTIGUOUS and ALIGNED."
+                    f"It has been automatically made contiguous and aligned."
+                )
+                initial_value = np.ascontiguousarray(initial_value)
+            self._data = initial_value
+
+
+    def get_linkable_array(self):
+        return self._data
+
+    @property
+    def data(self):
         """Returns a shared numpy array with the underlying data.
 
         Changes to the return value will be reflected in the astra
@@ -144,30 +194,16 @@ class Data(object):
         :rtype: np.array
 
         """
-        return astra.data3d.get_shared(self.astra_id)
+        return self._data
 
     @data.setter
     def data(self, val):
-        raise ValueError(
-            "You cannot change which numpy array backs a dataset.\n"
-            "To change the underlying data instead, use: \n"
-            " >>> vd.data[:] = new_data\n"
-        )
-
-    def is_volume(self):
-        return ts.geometry.is_volume(self.geometry)
-
-    def is_projection(self):
-        return ts.geometry.is_projection(self.geometry)
-
-    def to_astra(self):
-        """Returns astra data id associated with current object
-
-        :returns:
-        :rtype:
-
-        """
-        return self.astra_id
+        if val != self._data:
+            raise AttributeError(
+                "You cannot change which numpy array backs a dataset.\n"
+                "To change the underlying data instead, use: \n"
+                " >>> vd.data[:] = new_data\n"
+            )
 
 
 @ts.display.register(Data)

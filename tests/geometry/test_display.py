3bf2477e24595866083bf0afad14e056dcfc17fb
Allard Hendriksen <allard.hendriksen@cwi.nl>
2019-07-15 18:07:58 +0200

    Improve display color schemes for oriented box and geometries
    This scheme should be more amenable to show in GIFs and
    documentation.

--- tests/geometry/test_display.py
+++ tests/geometry/test_display.py
@@ -5,14 +5,13 @@ def test_display_parallel_geometry(interactive):
     pg = ts.parallel(angles=100, shape=100)
     vg = ts.volume()
     if interactive:
-        ts.display(pg, vg)
+        ts.display(vg, pg)
 
-
-def test_display_random_parallel_geometry(interactive):
-    pg = ts.geometry.random_transform()(ts.parallel(angles=100, shape=100).to_vec())
-    vg = ts.volume()
+    # Test with multiple geometries:
+    pg2 = ts.parallel(angles=10, size=2.0)
+    vg2 = ts.volume().translate(1.0)
     if interactive:
-        ts.display(pg, vg)
+        ts.display(vg, vg2, pg, pg2)
 
 
 def test_display_cone_geometry(interactive):
@@ -21,3 +20,8 @@ def test_display_cone_geometry(interactive):
 
     if interactive:
         ts.display(pg, vg)
+
+    # Test with two projection geometries:
+    pg2 = ts.cone(angles=50, src_obj_dist=5, src_det_dist=7.5)
+    if interactive:
+        ts.display(vg, pg, pg2)
--- tomosipo/geometry/display.py
+++ tomosipo/geometry/display.py
@@ -8,6 +8,66 @@ from .volume import VolumeGeometry, is_volume
 from .base_projection import ProjectionGeometry, is_projection
 
 
+def _pg_items(pg, colors, i):
+    pg = pg.to_vec()
+    i = i % pg.num_angles
+    src_curve_color, det_color, *_ = colors
+
+    # Drawing options:
+    curve_opts = dict(width=1, mode="line_strip", antialias=True)
+    det_curve_opts = dict(**curve_opts, color=det_color)
+    src_curve_opts = dict(**curve_opts, color=src_curve_color)
+    ray_opts = dict(**curve_opts, color=(0.0, 0.0, 0.0, .3))
+    det_opts = dict(smooth=True, drawEdges=True, color=det_color)
+
+    # Detector Curve
+    det_curve = gl.GLLinePlotItem(pos=pg.det_pos[:, ::-1], **det_curve_opts)
+
+    # Detector
+    # Get corners in XYZ coordinates:
+    corners = pg.corners[i, :, ::-1]
+    meshdata = np.array(list(itertools.product(corners, repeat=3)))
+    det_item = gl.GLMeshItem(vertexes=meshdata, **det_opts)
+
+    if pg.is_cone:
+        # Source curve
+        src_item = gl.GLLinePlotItem(pos=pg.src_pos[:, ::-1], **src_curve_opts)
+        # Rays
+        source = pg.src_pos[i, ::-1]
+        ray_items = [
+            gl.GLLinePlotItem(pos=np.array([source, corner]), **ray_opts)
+            for corner in corners
+        ]
+        return (src_item, *ray_items, det_curve, det_item)
+    elif pg.is_parallel:
+        # Rays
+        ray = pg.ray_dir[i, ::-1]  # XYZ order
+        ray *= 100 * np.linalg.norm(pg.det_sizes[0])
+        ray_items = [
+            gl.GLLinePlotItem(pos=np.array([corner - ray, corner + ray]), **ray_opts)
+            for corner in corners
+        ]
+        return (*ray_items, det_curve, det_item)
+
+    raise ValueError("Expected cone or parallel geometry")
+
+
+def _vg_item(vg, color):
+    corners = np.array(vg.get_corners())[:, ::-1]
+    volume_mesh = np.array(list(itertools.product(corners, corners, corners)))
+
+    return gl.GLMeshItem(
+        vertexes=volume_mesh, smooth=True, color=color, drawEdges=True, drawFaces=True
+    )
+
+
+def _take(xs, n):
+    r = []
+    for _ in range(n):
+        r.append(next(xs))
+    return r
+
+
 @ts.display.register(VolumeGeometry)
 @ts.display.register(ProjectionGeometry)
 def display_geometry(*geometries):
@@ -15,9 +75,8 @@ def display_geometry(*geometries):
 
     Note: requires the installation of pyopengl.
 
-    :param pg: ProjectionGeometry
-        Only cone beam geometries are supported currently.
-    :param vg: VolumeGeometry
+    :param geometries:
+        Any combination of volume and projection geometries.
     :returns: Nothing
     :rtype: None
 
@@ -26,137 +85,43 @@ def display_geometry(*geometries):
     pgs = [g for g in geometries if is_projection(g)]
     vgs = [g for g in geometries if is_volume(g)]
 
-    if len(pgs) != 1:
-        raise ValueError(
-            f"Expected 1 projection geometry to be supplied to ts.display_geometry. Got: {len(pgs)}"
-        )
-
-    if len(vgs) != 1:
-        raise ValueError(
-            f"Expected 1 volume geometry to be supplied to ts.display_geometry. Got: {len(vgs)}"
-        )
-
-    pg, *_ = pgs
-    vg, *_ = vgs
-
     app = get_app()
     view = gl.GLViewWidget()
-    # view.setBackgroundColor('w')
-
+    view.setBackgroundColor(0.95)
     view.show()
 
-    pg = pg.to_vec()
+    idx = []
+    for i in range(8):
+        idx = idx + list(range(i, 256, 32))
+    colors = map(tuple, rainbow_colormap[idx])
 
-    # Get source positions in X, Y, Z order
-    if pg.is_cone:
-        source_pos = pg.src_pos[:, ::-1]
-    if pg.is_parallel:
-        ray_dir = pg.ray_dir[:, ::-1]  # XYZ order
-    # Get detector corners shaped (num_angles, 4 -- for each corner,
-    # 3) and in XYZ order.
-    detector_corners = pg.corners[:, :, ::-1]
-    # Calculate detector center from the corners:
-    detector_origins = np.mean(detector_corners, axis=1)
-    #######################################################################
-    #                          Draw source curve                          #
-    #######################################################################
-    if pg.is_cone:
-        sourceItem = gl.GLLinePlotItem(pos=source_pos, width=1, mode="line_strip")
-        view.addItem(sourceItem)
+    for vg in vgs:
+        color, *colors = colors
+        view.addItem(_vg_item(vg, color))
 
-    #######################################################################
-    #                         Draw detector curve                         #
-    #######################################################################
-    # Detector positions in (X, Y, Z) formation
+    colors = itertools.cycle(colors)
+    pg_colors = [tuple(_take(colors, 2)) for _ in pgs]
 
-    detector_curve = gl.GLLinePlotItem(
-        pos=detector_origins, width=1, color=(0, 1, 0, 1), mode="line_strip"
-    )
-    view.addItem(detector_curve)
+    print(pg_colors)
+    assert len(pg_colors) == len(pgs)
 
-    #######################################################################
-    #                         Show volume geometry                        #
-    #######################################################################
-    # 8 corners in (XYZ) formation
-    corners = np.array(vg.get_corners())[:, ::-1]
-    volume_mesh = np.array(list(itertools.product(corners, corners, corners)))
-
-    volumeItem = gl.GLMeshItem(
-        vertexes=volume_mesh,
-        smooth=False,
-        color=(0.0, 1.0, 1.0, 1.0),
-        drawEdges=False,
-        drawFaces=True,
-    )
-    view.addItem(volumeItem)
-
-    #######################################################################
-    #                     Draw detector at first angle                    #
-    #######################################################################
-    # Show detector position i
-    def draw_detector(i):
-        i = i % len(detector_origins)
-
-        corners = detector_corners[i]
-        meshdata = np.array(list(itertools.product(corners, repeat=3)))
-
-        detectorItem = gl.GLMeshItem(
-            vertexes=meshdata, smooth=False, color=(1.0, 0, 0, 0.9), drawEdges=False
-        )
-        return detectorItem
-
-    #######################################################################
-    #                  Draw rays through detector corners                 #
-    #######################################################################
-
-    def draw_corner_rays(i):
-        i = i % len(detector_origins)
-
-        corners = detector_corners[i]
-        if pg.is_cone:
-            source = np.copy(source_pos[i])
-            rayItems = [
-                gl.GLLinePlotItem(
-                    pos=np.array([source, corner]), width=1, mode="line_strip"
-                )
-                for corner in corners
-            ]
-        elif pg.is_parallel:
-            ray = np.copy(ray_dir[i])
-            ray *= 10 * np.linalg.norm(pg.det_sizes[0])
-            rayItems = [
-                gl.GLLinePlotItem(
-                    pos=np.array([corner - 10 * ray, corner + 10 * ray]),
-                    width=1,
-                    mode="line_strip",
-                )
-                for corner in corners
-            ]
-
-        return rayItems
-
-    ray_items = None
-    detector_item = None
+    tmp_items = []
     i = 0
 
     def on_timer():
-        nonlocal ray_items, detector_item, i
-        if ray_items is not None:
-            for item in ray_items:
-                view.removeItem(item)
-            ray_items = None
-        if detector_item is not None:
-            view.removeItem(detector_item)
-            detector_item = None
-        ray_items = draw_corner_rays(i)
-        detector_item = draw_detector(i)
-        view.addItem(detector_item)
-        for r in ray_items:
-            view.addItem(r)
+        nonlocal i, tmp_items, pg_colors
+        for item in tmp_items:
+            view.removeItem(item)
+        tmp_items = []
+        for pg, c in zip(pgs, pg_colors):
+            for item in _pg_items(pg, c, i):
+                view.addItem(item)
+                tmp_items.append(item)
         i += 1
 
     timer = QtCore.QTimer()
     timer.timeout.connect(on_timer)
-    timer.start(5000 / len(detector_origins))
+    max_angles = max(pg.num_angles for pg in pgs)
+    timer.start(5000 / max_angles)
     on_timer()
     app.exec_()

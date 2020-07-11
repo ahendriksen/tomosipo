bbc496dc6f891a981fea7c4dc62288d6478f8255
Johannes Leuschner <jleuschn@uni-bremen.de>
2020-07-12 01:01:46 +0200

    put z axis first in odl converters

--- tomosipo/odl.py
+++ tomosipo/odl.py
@@ -83,32 +83,33 @@ def fan_to_cone_beam_geometry(fb_geometry, det_z_span=1.):
         if not isinstance(fb_geometry, odl.tomo.FanBeamGeometry):
             raise TypeError('expected a `FanBeamGeometry`')
     apart = fb_geometry.motion_partition
-    det_min_pt = [fb_geometry.det_partition.min_pt[0], -det_z_span/2.]
-    det_max_pt = [fb_geometry.det_partition.max_pt[0], +det_z_span/2.]
+    det_min_pt = [-det_z_span/2., fb_geometry.det_partition.min_pt[0]]
+    det_max_pt = [+det_z_span/2., fb_geometry.det_partition.max_pt[0]]
     dpart = odl.discr.RectPartition(
         odl.set.IntervalProd(det_min_pt, det_max_pt),
-        odl.discr.RectGrid(fb_geometry.det_partition.coord_vectors[0], [0.]))
+        odl.discr.RectGrid([0.], fb_geometry.det_partition.coord_vectors[0]))
     src_radius = fb_geometry.src_radius
     det_radius = fb_geometry.det_radius
     if version.parse(odl.__version__) > version.parse('0.7.0'):
         det_curvature_radius = (
             None if fb_geometry.det_curvature_radius is None
             else (fb_geometry.det_curvature_radius, None))
-    src_to_det_init = [fb_geometry.src_to_det_init[0],
-                       fb_geometry.src_to_det_init[1],
-                       0.]
-    det_axes_init = ([fb_geometry.det_axis_init[0],
-                      fb_geometry.det_axis_init[1],
-                      0.],
-                     [0., 0., 1.])
-    translation = [fb_geometry.translation[0],
-                   fb_geometry.translation[1],
-                   0.]
+    src_to_det_init = [0.,
+                       fb_geometry.src_to_det_init[0],
+                       fb_geometry.src_to_det_init[1]]
+    det_axes_init = ([1., 0., 0.],
+                     [0.,
+                      fb_geometry.det_axis_init[0],
+                      fb_geometry.det_axis_init[1]],
+                     )
+    translation = [0.,
+                   fb_geometry.translation[0],
+                   fb_geometry.translation[1]]
     if version.parse(odl.__version__) <= version.parse('0.7.0'):
         cb_geometry = odl.tomo.ConeFlatGeometry(
             apart, dpart, src_radius, det_radius,
             pitch=0.,
-            axis=[0., 0., 1.],
+            axis=[1., 0., 0.],
             src_to_det_init=src_to_det_init,
             det_axes_init=det_axes_init,
             translation=translation,
@@ -119,7 +120,7 @@ def fan_to_cone_beam_geometry(fb_geometry, det_z_span=1.):
         apart, dpart, src_radius, det_radius,
         det_curvature_radius=det_curvature_radius,
         pitch=0.,
-        axis=[0., 0., 1.],
+        axis=[1., 0., 0.],
         src_to_det_init=src_to_det_init,
         det_axes_init=det_axes_init,
         translation=translation,
@@ -170,33 +171,33 @@ def parallel_2d_to_3d_geometry(pb2d_geometry, det_z_shape=1, det_z_span=None):
         det_z_span = (-det_z_span/2., det_z_span/2.)
     det_z_partition = odl.uniform_partition(
         min_pt=det_z_span[0], max_pt=det_z_span[1], shape=det_z_shape)
-    det_min_pt = [pb2d_geometry.det_partition.min_pt[0],
-                  det_z_partition.min_pt[0]]
-    det_max_pt = [pb2d_geometry.det_partition.max_pt[0],
-                  det_z_partition.max_pt[0]]
+    det_min_pt = [det_z_partition.min_pt[0],
+                  pb2d_geometry.det_partition.min_pt[0]]
+    det_max_pt = [det_z_partition.max_pt[0],
+                  pb2d_geometry.det_partition.max_pt[0]]
     dpart = odl.discr.RectPartition(
         odl.set.IntervalProd(det_min_pt, det_max_pt),
-        odl.discr.RectGrid(pb2d_geometry.det_partition.coord_vectors[0],
-                           det_z_partition.coord_vectors[0]))
+        odl.discr.RectGrid(det_z_partition.coord_vectors[0],
+                           pb2d_geometry.det_partition.coord_vectors[0]))
     # The RayTransform in 2d seems to act agnostic of the parameters
     # `det_pos_init`, `det_axes_init` and `translation`.
     # Seems to me like a bug, could be related to
     # https://github.com/odlgroup/odl/issues/359),
     # The 3d transform would handle them, so the results will be inconsistent.
     # Therefore i also did not test the following definitions.
-    det_pos_init = [pb2d_geometry.det_pos_init[0],
-                    pb2d_geometry.det_pos_init[1],
-                    0.]
-    det_axes_init = ([pb2d_geometry.det_axis_init[0],
-                      pb2d_geometry.det_axis_init[1],
-                      0.],
-                      [0., 0., 1.])
-    translation = [pb2d_geometry.translation[0],
-                   pb2d_geometry.translation[1],
-                   0.]
+    det_pos_init = [0.,
+                    pb2d_geometry.det_pos_init[0],
+                    pb2d_geometry.det_pos_init[1]]
+    det_axes_init = ([1., 0., 0.],
+                     [0.,
+                      pb2d_geometry.det_axis_init[0],
+                      pb2d_geometry.det_axis_init[1]])
+    translation = [0.,
+                   pb2d_geometry.translation[0],
+                   pb2d_geometry.translation[1]]
     pb3d_geometry = odl.tomo.Parallel3dAxisGeometry(
         apart, dpart,
-        axis=[0., 0., 1.],
+        axis=[1., 0., 0.],
         det_pos_init=det_pos_init,
         det_axes_init=det_axes_init,
         translation=translation,
@@ -237,17 +238,17 @@ def discretized_space_2d_to_3d(space, z_shape=1, z_span=None):
         min_pt=z_span[0], max_pt=z_span[1], shape=z_shape)
     rect_partition = odl.discr.RectPartition(
             odl.set.IntervalProd(
-                [space.min_pt[0],
-                 space.min_pt[1],
-                 z_partition.min_pt[0]],
-                [space.max_pt[0],
-                 space.max_pt[1],
-                 z_partition.max_pt[0]]),
-            odl.discr.RectGrid(space.partition.coord_vectors[0],
-                               space.partition.coord_vectors[1],
-                               z_partition.coord_vectors[0]))
+                [z_partition.min_pt[0],
+                 space.min_pt[0],
+                 space.min_pt[1]],
+                [z_partition.max_pt[0],
+                 space.max_pt[0],
+                 space.max_pt[1]]),
+            odl.discr.RectGrid(z_partition.coord_vectors[0],
+                               space.partition.coord_vectors[0],
+                               space.partition.coord_vectors[1]))
     tensor_space = odl.space.space_utils.tensor_space(
-            (space.tspace.shape[0], space.tspace.shape[1], z_shape),
+            (z_shape, space.tspace.shape[0], space.tspace.shape[1]),
             dtype=space.tspace.dtype,
             impl=space.tspace.impl)
     if version.parse(odl.__version__) <= version.parse('0.7.0'):
@@ -257,12 +258,12 @@ def discretized_space_2d_to_3d(space, z_shape=1, z_span=None):
                 'for odl <= 0.7.0 only `DiscreteLp` spaces on `IntervalProd` '
                 'sets are supported')
         function_space = odl.FunctionSpace(
-            odl.IntervalProd([space.fspace.domain.min_pt[0],
-                              space.fspace.domain.min_pt[1],
-                              z_partition.min_pt[0]],
-                             [space.fspace.domain.max_pt[0],
-                              space.fspace.domain.max_pt[1],
-                              z_partition.max_pt[0]]),
+            odl.IntervalProd([z_partition.min_pt[0],
+                              space.fspace.domain.min_pt[0],
+                              space.fspace.domain.min_pt[1]],
+                             [z_partition.max_pt[0],
+                              space.fspace.domain.max_pt[0],
+                              space.fspace.domain.max_pt[1]]),
             out_dtype=space.fspace.out_dtype
             )
         space3d = odl.discr.DiscreteLp(

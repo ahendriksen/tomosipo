29e2b21cb7f81c40f0a9838008ce663d968c4b16
Allard Hendriksen <allard.hendriksen@cwi.nl>
2021-04-26 16:06:15 +0200

    tests: Apply formatting

--- tests/test_svg.py
+++ tests/test_svg.py
@@ -3,16 +3,23 @@
 import tomosipo as ts
 import numpy as np
 
+
 def test_svg():
     geoms = [
-        ts.cone(angles=10, cone_angle=1/2).to_vec(),
+        ts.cone(angles=10, cone_angle=1 / 2).to_vec(),
         ts.parallel(angles=3).to_vec(),
         ts.volume().to_vec(),
     ]
 
     options = [
         dict(height=100, width=100, duration=3, camera=None, show_axes=True),
-        dict(height=100, width=100, duration=3, camera=ts.cone(shape=100, cone_angle=1/2), show_axes=True),
+        dict(
+            height=100,
+            width=100,
+            duration=3,
+            camera=ts.cone(shape=100, cone_angle=1 / 2),
+            show_axes=True,
+        ),
         dict(height=20, width=20, duration=3, camera=None, show_axes=False),
         dict(height=20, width=20, duration=1.0, camera=None, show_axes=False),
     ]
